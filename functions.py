fc842359da15658ef9a0d60f3eccc0ac272a34b5
vdella <vitor.origamer@gmail.com>
2024-10-04 11:46:22 -0300

    generalized activation functions and output functions

--- functions.py
+++ functions.py
@@ -7,3 +7,25 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
+
+
+def softmax(x):
+    """the softmax applies the standard exponential function to each element z[i] of the input vector z
+    (consisting of K real numbers), and normalizes these values by dividing by the sum of all these exponentials."""
+    exps = np.exp(x - x.max(axis=1, keepdims=True))
+    return exps / np.sum(exps, axis=1, keepdims=True)
+
+
+hidden_activation = {
+    'sigmoid': (sigmoid, sigmoid_derivative),
+    'relu': (relu, relu_derivative)
+}
+output_activation = {'sigmoid': sigmoid, 'softmax': softmax}
--- network.py
+++ network.py
@@ -1,12 +1,17 @@
-from functions import *
+from functions import hidden_activation, output_activation
+import numpy as np
 
 
 class NeuralNetwork:
 
-    def __init__(self, layers: list, learning_rate=0.01):
-        """ Gets a list of the number of neurons in each
-        layer of the network and the learning rate. Initializes
-        the weights and biases for each layer."""
+    def __init__(self, layers: list, learning_rate=0.01, hidden_act='sigmoid', output_act='sigmoid'):
+        """Standard fully connected neural network initialization.
+
+        :param layers: list of integers, where each integer represents the number of neurons in each layer.
+        :param learning_rate: float, the learning rate of the network.
+        :param hidden_act: string, the activation function to be used in the hidden layers.
+        :param output_act: string, the activation function to be used in the output layer.
+        """
 
         self.layers = layers
         self.parameters = {}
@@ -15,28 +20,42 @@ class NeuralNetwork:
             self.parameters[f'W{l}'] = np.random.randn(layers[l-1], layers[l]) * 0.01
             self.parameters[f'b{l}'] = np.zeros((1, layers[l]))
 
+        self.hidden_act = hidden_activation[hidden_act][0]
+        self.hidden_act_derivative = hidden_activation[hidden_act][1]
+
+        self.output_act = output_activation[output_act]
+
         self.learning_rate = learning_rate
-        self.losses = []  # Records losses along the epochs in order to plot them later.
+        self.losses = {}  # Records losses along the epochs in order to plot them later.
 
     def forward(self, X):
         self.parameters[f'Z1'] = (np.dot(X, self.parameters[f'W1'])
                                   + self.parameters[f'b1'])
-        self.parameters[f'A1'] = sigmoid(self.parameters[f'Z1'])
+        self.parameters[f'A1'] = self.hidden_act(self.parameters[f'Z1'])
 
         for l in range(2, len(self.layers)):
             self.parameters[f'Z{l}'] = (np.dot(
                 self.parameters[f'A{l-1}'], self.parameters[f'W{l}'])
                                         + self.parameters[f'b{l}'])
-            self.parameters[f'A{l}'] = sigmoid(self.parameters[f'Z{l}'])
+            self.parameters[f'A{l}'] = self.hidden_act(self.parameters[f'Z{l}'])
 
         return self.parameters[f'A{len(self.layers) - 1}']
 
     @staticmethod
     def compute_loss(Y, Y_hat):
-        # Cross-entropy loss
-        m = Y.shape[0]
-        log_probs = -np.log(Y_hat[range(m), Y.argmax(axis=1)])
-        loss = np.sum(log_probs) / m
+        """
+        Computes the cross-entropy loss between the true labels and predicted probabilities.
+
+        :param Y: numpy array containing the true labels.
+        :param Y_hat: numpy array containing the predicted probabilities.
+        :return: Cross-entropy loss value.
+        """
+        # Clip the predicted values to a small value in order to avoid log(0) errors.
+        Y_hat = np.clip(Y_hat, 1e-15, 1 - 1e-15)
+
+        # Compute the cross-entropy loss
+        loss = -np.mean(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
+
         return loss
 
     def backward(self, X, Y, Y_hat):
@@ -50,9 +69,8 @@ class NeuralNetwork:
             else:
                 self.parameters[f'dA{l}'] = np.dot(self.parameters[f'dZ{l + 1}'],
                                                    self.parameters[f'W{l + 1}'].T)
-                self.parameters[f'dZ{l}'] = \
-                    (self.parameters[f'dA{l}']
-                    * sigmoid_derivative(self.parameters[f'Z{l}']))
+                self.parameters[f'dZ{l}'] = (self.parameters[f'dA{l}']
+                    * self.hidden_act_derivative(self.parameters[f'Z{l}']))
                 self.parameters[f'dW{l}'] = np.dot(self.parameters[f'A{l - 1}'].T,
                                                    self.parameters[f'dZ{l}']) \
                     if l > 1 else np.dot(X.T, self.parameters[f'dZ{l}'])
@@ -64,15 +82,12 @@ class NeuralNetwork:
             self.parameters[f'b{l}'] -= self.learning_rate * self.parameters[f'db{l}']
 
     def train(self, X, Y, epochs=1000):
-        # Train the network
+
         for i in range(epochs):
             Y_hat = self.forward(X)
             loss = self.compute_loss(Y, Y_hat)
-            self.losses.append(loss)
+            self.losses[i] = loss
             self.backward(X, Y, Y_hat)
-
-            if i % 100 == 0:
-                print(f"Epoch {i}, Loss: {loss:.4f}")
 
     def predict(self, X):
         Y_hat = self.forward(X)
